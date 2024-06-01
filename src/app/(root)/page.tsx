b294ffcd2f67c8b09ff85c338110db7a339a1dba
Abhijeet Kumar <133308751+403ABHIJEET@users.noreply.github.com>
2024-06-01 11:21:37 +0530

    made auth pages

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,12 @@
+'use client'
 import Image from "next/image";
+import { Button } from '@/components/ui/button'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <>dahbaoard</>
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   );
 }
