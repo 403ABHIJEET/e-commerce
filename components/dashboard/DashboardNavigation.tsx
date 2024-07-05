66002c18e20a2f8c828679b6fd4800dc23b1d068
Abhijeet Kumar <133308751+403ABHIJEET@users.noreply.github.com>
2024-07-05 11:20:54 +0530

    uploadthings

--- components/dashboard/DashboardNavigation.tsx
+++ components/dashboard/DashboardNavigation.tsx
@@ -1,32 +1,40 @@
+'use client'
+import { cn } from '@/lib/utils'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
+
+const links = [
+  {
+    name: 'Dashboard',
+    href: '/dashboard'
+  },
+  {
+    name: 'Orders',
+    href: '/dashboard/orders',
+  },
+  {
+    name: 'Products',
+    href: '/dashboard/products'
+  },
+  {
+    name: 'Category',
+    href: '/dashboard/category'
+  }
+]
 
 const DashboardNavigation = () => {
 
-  const links = [
-    {
-      name: 'Dashboard',
-      href: '/dashboard'
-    },
-    {
-      name: 'Orders',
-      href: '/dashboard/orders',
-    },
-    {
-      name: 'Products',
-      href: '/dashboard/products'
-    },
-    {
-      name: 'Category',
-      href: '/dashboard/category'
-    }
-  ]
+  const pathname = usePathname()
 
   return (
     <>
       {
         links.map((item) => (
-          <Link key={item.name} href={item.href}>
+          <Link key={item.name} href={item.href} className={cn(
+            item.href === pathname ? "text-black" : 
+            "text-muted-foreground hover:text-foreground hover:scale-125 transition-all ease-in-out"
+          )}>
             {item.name}
           </Link>
         )) 
